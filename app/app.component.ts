2c3b0f5e58f6362078877d26d4508df4315fa3ef
Christine Tacha <tacha.christine@gmail.com>
2016-08-11 08:57:38 -0700

    add resources files

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from 'angular2/core';
   selector: 'my-app',
   template: `
   <div class="container">
-    <h1>Skeleton Angular2 App</h1>
+    <h1>Jackpot Records</h1>
   </div>
   `
 })
