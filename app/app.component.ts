c1edbde2c8494e4f5f5dcc433df1a4aa6b09d736
Christine Tacha <tacha.christine@gmail.com>
2016-08-12 08:55:42 -0700

    add debug log to .gitignore, inital file setup, new title from skeleton

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,26 +4,13 @@ import { Component } from 'angular2/core';
   selector: 'my-app',
   template: `
   <div class="container">
-    <h1>Jackpot Record Inventory</h1>
-    <h3 *ngFor="#album of albums"> {{ album.name }} </h3>
+    <h1>Meal Tracker</h1>
   </div>
   `
 })
 
 export class AppComponent {
-  public album: Album[];
-  constructor() {
-    this.albums = [
-      new Album("Red", 0),
-      new Album("Blue", 1),
-      new Album("Yellow", 2)
-    ];
-  }
 }
 
 export class Album {
-  public album: boolean = false;
-  constructor(public name: string, public artist: string, public price: number, public genre: string) {
-
-  }
 }
